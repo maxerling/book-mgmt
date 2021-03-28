37f949a063236c2e56dba1ef2f565bf18c30efc5
maxerling <maximilianerling@gmail.com>
2021-03-28 12:43:54 +0200

    first commit

---
+++ src/main/java/Author.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Max Erling
+ * Date: 2021-03-27
+ * Copyright: MIT
+ * Class: Java20B
+ */
+public class Author {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+}
---
+++ src/main/java/Book.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+/**
+ * Created by Max Erling
+ * Date: 2021-03-27
+ * Copyright: MIT
+ * Class: Java20B
+ */
+public class Book {
+    private int id;
+    private String title;
+    private int price;
+    private List<Author> authors;
+}
